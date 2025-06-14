d12983ed6695011b743ac9e8a59eebef91dc44de
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2025-06-14 22:19:47 +0200

    as

--- src/App.py
+++ src/App.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from pages.MainPage import MainScreen
 from pages.HistoryPage import HistoryScreen
-from pages.MediaPlayer.MediaRoutes import MediaRoutes
 from Database.Script.Scrpit import executeDatabase 
 
 class DownloaderApp(App):
@@ -11,7 +10,6 @@ class DownloaderApp(App):
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='MainPage'))
         sm.add_widget(HistoryScreen(name='HistoryPage'))  
-        MediaRoutes(sm)
         return sm
 
 if __name__ == "__main__":
